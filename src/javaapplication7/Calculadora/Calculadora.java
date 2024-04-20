aad2f9e23fabf689ac47630c6f6230ac09ce253d
JeremyLeuyacc784 <166246425+JeremyLeuyacc784@users.noreply.github.com>
2024-04-20 14:09:05 -0500

    Add files via upload

---
+++ src/javaapplication7/Calculadora/Calculadora.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication7.Calculadora;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Calculadora {
+    float valor1;
+    float valor2;   
+    public Calculadora(float valor1, float valor2) {
+        this.valor1 = valor1;
+        this.valor2 = valor2;
+    }
+    public float sumar(){ 
+        return this.valor1 + this.valor2;
+    }
+    public float restar(){
+        return this.valor1-this.valor2;
+    }
+    public float multiplicar(){
+      return this.valor1 * this.valor2;
+    }
+    public float dividir(){
+        return this.valor1 / this.valor2;
+    }
+}
